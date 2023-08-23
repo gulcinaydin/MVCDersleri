582c659a54778900ae4926d9834ab48ac40e98c7
gulcinaydin <gulcinaydin11d@gmail.com>
2023-08-23 03:12:18 +0300

    web api token

---
+++ WebApiToken/Models/KutuphaneServis.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApiToken.Models
+{
+    public class KutuphaneServis : DbContext
+    {
+        public KutuphaneServis() : base("name=KutuphaneEntity")
+        {
+
+        }
+
+        public DbSet<Kitap> Kitaplar { get; set; }
+    }
+}
